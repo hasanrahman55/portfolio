27b6a2be8a58140295d600649faf459435ec4713
Hasan Rahman <hasanrahman33605@gmail.com>
2024-11-27 17:49:53 +0600

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
-      <NavBar />
-    </>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selected:bg-cyan-300 selected:text-neutral-900">
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
+
+      <div className="container mx-auto px-8">
+        <NavBar />
+      </div>
+    </div>
   );
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,7 +8,7 @@ function NavBar() {
   return (
     <nav className="bg-red-200 mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="h-8 w-8" src={logo} alt="Hasan Rahman Logo" />
+        <img className="mx-2 w-10" src={logo} alt="Hasan Rahman Logo" />
       </div>
 
       <div className="m-8 flex justify-center items-center gap-4 text-2xl">
