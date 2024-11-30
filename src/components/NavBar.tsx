1805f01d45028a3050d19dc69e308467483ddd2e
Hasan Rahman <hasanrahman33605@gmail.com>
2024-11-30 20:09:37 +0600

    final touch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,10 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/logo.png";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import resume from "../assets/resume.pdf";
 
-import { FaInstagram } from "react-icons/fa";
 function NavBar() {
   return (
     <nav className=" mb-20 flex items-center justify-between py-6">
@@ -12,10 +13,26 @@ function NavBar() {
       </div>
 
       <div className="m-8 flex justify-center items-center gap-4 text-2xl">
-        <FaLinkedinIn />
-        <FaGithub />
-        <FaXTwitter />
-        <FaInstagram />
+        <a
+          href="https://www.linkedin.com/in/hasanrahman55/"
+          target="_blank"
+          title="Linkedin"
+        >
+          <FaLinkedinIn />
+        </a>
+        <a
+          href="https://github.com/hasanrahman55"
+          target="_blank"
+          title="Github"
+        >
+          <FaGithub />
+        </a>
+        <a href="https://x.com/hasanrahman55" target="_blank" title="Twitter">
+          <FaXTwitter />
+        </a>
+        <a href={resume} download={true} target="_blank" title="Resume">
+          <IoDocumentTextOutline />
+        </a>
       </div>
     </nav>
   );
